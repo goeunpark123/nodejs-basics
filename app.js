5dc11b7b41d9c7f010ebeb8ca971ec6dc7606243
goeunpark123 <goeunpark21@gmail.com>
2023-08-13 23:27:09 +0900

    feat post

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-const path = require("path");
 const bodyParser = require("body-parser");
 
 const express = require("express");
 const app = express();
 
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const feedRoutes = require("./routes/feed");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/admin", adminData.routes);
-app.use(shopRoutes);
-
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-});
+app.use(bodyParser.json());
+app.use("/feed", feedRoutes);
 
 app.listen(3000);
