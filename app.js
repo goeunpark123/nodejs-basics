cc36b8f22afca012be417f1686c184f6504eb913
goeunpark123 <goeunpark21@gmail.com>
2023-08-26 21:40:37 +0900

    use express

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-const bodyParser = require("body-parser");
-
 const express = require("express");
 const app = express();
 
-const feedRoutes = require("./routes/feed");
+app.use((req, res, next) => {
+  let body = "";
+  req.on("end", () => {
+    //2
+    const userName = body.split("=")[1];
+    if (userName) {
+      req.body = { name: userName };
+    }
+    console.log(body + "2");
+    next();
+  });
+  req.on("data", (chunk) => {
+    //1
+    body += chunk;
+  });
+});
 
-app.use(bodyParser.json());
-app.use("/feed", feedRoutes);
+app.use((req, res, next) => {
+  //second middleware
+  if (req.body) {
+    return res.send("<h1>" + req.body.name + "</h1>");
+  }
+  res.send(
+    '<form method="POST"><input type="text" name="userName"><button type="text">check</button>'
+  );
+});
 
-app.listen(3000);
+app.listen(5000);
