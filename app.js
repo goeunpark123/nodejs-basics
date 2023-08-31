8ea6aebd092d48bf2ddb2b8d82b68e72020283a8
goeunpark123 <goeunpark21@gmail.com>
2023-09-01 00:20:02 +0900

    handling error

--- app.js
+++ app.js
@@ -6,5 +6,14 @@ const userRoutes = require("./routes/user-routes");
 const app = express();
 
 app.use("/api/places", placeRoutes);
-app.use("/api/users", userRoutes);
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({ message: error.message || "Unknown error" });
+});
+
+//app.use("/api/users", userRoutes);
 app.listen(5000);
