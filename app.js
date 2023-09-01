af6dada934ef562aa976ec9329555619a320be67
goeunpark123 <goeunpark21@gmail.com>
2023-09-02 00:32:12 +0900

    feat update

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const placeRoutes = require("./routes/place-routes");
 const userRoutes = require("./routes/user-routes");
+
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json()); //.urlencoded()
 
 app.use("/api/places", placeRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error; //default error handler에서 메세지 출력
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
--- controllers/place-controller.js
+++ controllers/place-controller.js
@@ -60,6 +60,25 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeid = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeid) };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeid);
+
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => {};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
